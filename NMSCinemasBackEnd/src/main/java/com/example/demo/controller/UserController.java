cb0298c484116f4c35cef228144dd2829b2b9961
saadkhan7500 <mohammadsaad7500@gmail.com>
2023-04-20 01:48:24 +0530

    added user service

---
+++ NMSCinemasBackEnd/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,77 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.User;
+import com.example.demo.service.UserService;
+import com.example.demo.userAuthentication.UserAuthentication;
+
+
+@CrossOrigin
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserService service;
+	
+	
+	//API to add the new user at the time of registration
+	@PostMapping("/addUser")
+	public User addUser(@RequestBody User user) throws InterruptedException
+	{
+		System.out.println("Add user API calling");
+		Thread.sleep(2000);
+	    return service.addUser(user);
+	}
+	
+	//API to get the user by email
+	@GetMapping("getUser/{email}")
+	public User getUserByEmail(@PathVariable String email)
+	{
+		
+		System.out.println("Get user API calling "+email);
+	    return service.getUserByEmail(email);
+	}
+	
+	//API to authenticate the user at the time of login
+	@PostMapping("/checkUser")
+	public User AuthenticateUser(@RequestBody UserAuthentication user)
+	{
+		System.out.println("Authenticate user API calling");
+		
+		User u = service.AuthenticateUser(user);
+		
+		return u;
+	}
+	
+	@GetMapping("/getAllUsers")
+	public List<User> getAllUsers()
+	{
+		System.out.println("Get all user API calling");
+		return service.getAllUsers();
+	}
+	
+	//Edit User API
+	@PutMapping("/editUser")
+	public User editUser(@RequestBody User user)
+	{
+		return service.editUser(user);
+	}
+	
+	//Delete User API
+	@DeleteMapping("deleteUser/{email}")
+	String deleteUser(@PathVariable String email)
+	{
+	     return service.deleteUser(email);
+	}
+}
---
+++ NMSCinemasBackEnd/src/main/java/com/example/demo/entities/User.java
@@ -0,0 +1,103 @@
+package com.example.demo.entities;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class User 
+{
+	@Id
+	private String email;
+	private String name;
+	private String dob;
+	private String gender;
+	private String pno;
+	private String password;
+	
+	@OneToOne(targetEntity = UserAddress.class,cascade = CascadeType.ALL)
+    private UserAddress userAddress;
+	
+	private User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(String email, String name, String dob, String gender, String pno, String password,
+			UserAddress userAddress) {
+		super();
+		this.email = email;
+		this.name = name;
+		this.dob = dob;
+		this.gender = gender;
+		this.pno = pno;
+		this.password = password;
+		this.userAddress = userAddress;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getPno() {
+		return pno;
+	}
+
+	public void setPno(String pno) {
+		this.pno = pno;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserAddress getUserAddress() {
+		return userAddress;
+	}
+
+	public void setUserAddress(UserAddress userAddress) {
+		this.userAddress = userAddress;
+	}
+
+	@Override
+	public String toString() {
+		return "User [email=" + email + ", name=" + name + ", dob=" + dob + ", gender=" + gender + ", pno=" + pno
+				+ ", password=" + password + ", userAddress=" + userAddress + "]";
+	}
+
+	
+}
