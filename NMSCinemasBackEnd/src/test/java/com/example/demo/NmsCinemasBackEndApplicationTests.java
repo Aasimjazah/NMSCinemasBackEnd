abf9a5766f6b9e811466279dd227a40d9c072bb0
Aasimjazah <asimjazah8@gmail.com>
2023-04-20 00:54:53 +0530

    First commit from STS

---
+++ NMSCinemasBackEnd/src/test/java/com/example/demo/NmsCinemasBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmsCinemasBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
