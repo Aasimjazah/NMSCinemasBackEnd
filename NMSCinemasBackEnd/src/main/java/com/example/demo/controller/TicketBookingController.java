62110773f841bf76f2276524a842d6eaadeb4073
saadkhan7500 <mohammadsaad7500@gmail.com>
2023-04-26 15:27:03 +0530

    added functionality to show tickets to the user

--- NMSCinemasBackEnd/src/main/java/com/example/demo/controller/TicketBookingController.java
+++ NMSCinemasBackEnd/src/main/java/com/example/demo/controller/TicketBookingController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +25,12 @@ public class TicketBookingController {
 	public TicketBoooking addBooking(@RequestBody TicketBoooking ticketBoooking)
 	{
 		return bookingService.addTicket(ticketBoooking);
+	}
+	
+	@GetMapping("/getTicketsByEmail/{email}")
+	public List<TicketBoooking> getTicketsByEmail(@PathVariable("email") String email)
+	{
+		return bookingService.getTicketsByEmail(email);
 	}
 
 }
--- NMSCinemasBackEnd/src/main/java/com/example/demo/service/TicketBookingService.java
+++ NMSCinemasBackEnd/src/main/java/com/example/demo/service/TicketBookingService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,4 +20,9 @@ public class TicketBookingService {
 		return bookingRepo.save(ticketBoooking);
 	}
 
+	
+	public List<TicketBoooking> getTicketsByEmail(String email)
+	{
+		return bookingRepo.findByUserId(email);
+	}
 }
